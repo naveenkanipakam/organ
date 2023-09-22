267a8cfe9518efca66a432a1ce638c0bb25af21f
Naveen <naveenpillai015@gmail.comgit config --global user.name NaveenOmit --global to set the identity only in this repository.git commit -mAssignmentgit config --global user.email naveenpillai015@gmail.com>
2023-09-22 14:10:13 +0530

    donation

---
+++ script.js
@@ -0,0 +1,5 @@
+// JavaScript functionality (e.g., handling button click)
+document.getElementById("donateButton").addEventListener("click", function() {
+    // Redirect to the registration page
+    window.location.href = "registration.html";
+});
